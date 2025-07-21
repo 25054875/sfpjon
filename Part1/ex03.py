1f797a52abad3d3566c960d281e6bfc78e0f944b
25054875 <25054875@imail.sunway.edu.my>
2025-07-21 14:34:28 +0000

    Coding

---
+++ Part1/ex03.py
@@ -0,0 +1,11 @@
+string1= "MaRviN THE bot"
+print(string1)
+
+string_lower= "marvin the bot"
+print(string_lower)
+
+string_upper= "MARVIN THE BOT"  
+print(string_upper)
+
+string_nospace="MaRviN THE bot"  
+print(string_nospace)
