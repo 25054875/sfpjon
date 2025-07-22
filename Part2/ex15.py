97c05dd2ec51a3427d19d4453c2b289a3024b351
25054875 <25054875@imail.sunway.edu.my>
2025-07-22 03:23:48 +0000

    Coding

---
+++ Part2/ex15.py
@@ -0,0 +1,9 @@
+import random
+
+flowers = ['rose', 'tulip', 'lily']
+
+# 1. Print a random flower
+print(random.choice(flowers))
+
+# 2. Print a random number from 0 to 100
+print(random.randint(0, 100))
